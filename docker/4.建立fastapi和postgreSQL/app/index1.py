721ec0c33c93f5c83a9aecc889154e26f64103ec
roberthsu2003 <roberthsu2003@gmail.com>
2024-02-02 16:08:24 +0800

    modify

--- docker/4.建立fastapi和postgreSQL/app/index1.py
+++ docker/4.建立fastapi和postgreSQL/app/index1.py
@@ -1,3 +1,8 @@
 import streamlit as st
+import psycopg2
 
 st.write("Hello **world**!")
+
+conn = psycopg2.connect(database="postgres", user="postgres", 
+                        password="postgres", host="postgres", 
+                        port="5432")
